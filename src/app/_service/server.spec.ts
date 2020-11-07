9d6bd833c8755f6d2d91843a704de133ed84e8c7
Manuel relative Simon <manuel@fam-simon.de>
2020-11-07 11:50:48 +0100

    Server erstellt kleine Änderungen

--- src/app/_service/server.spec.ts
+++ src/app/_service/server.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DbConnectService } from './dbconnect.service';
+import { Server } from './server';
 
-describe('DbconnectService', () => {
+describe('ServerService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: DbConnectService = TestBed.get(DbConnectService);
+    const service: Server = TestBed.get(Server);
     expect(service).toBeTruthy();
   });
 });
